adf65f66f1b236fdc7b37ebf3b83330676da36e5
Noah <cramernoah8@gmail.com>
2024-08-30 19:46:46 -0400

    Add files via upload

---
+++ NovaTale/scripts/SceneTrigger.gd
@@ -0,0 +1,4 @@
+class_name SceneTrigger1 extends Area2D
+
+@export var connected_scene: String
+
---
+++ NovaTale/scripts/cursormenu.gd
@@ -0,0 +1,5 @@
+extends TextureRect
+
+@export var menu_parent_path : NodePath
+
+@onready var menu_parent := get_node(menu_parent_path)
