d13b1149fe10fc3e4dc51dd7112a9ac6bc2da263
Noah <cramernoah8@gmail.com>
2024-08-30 15:56:19 -0400

    Add files via upload
    This is the project as of right now.
    
    This is made on godot version 4.2
    
    Note:
    This does have *errors* when changing rooms and the continue button is disabled until i can figure out how to make game saves and access those saves. i
    
    if there is an easier way to do ANY of the things i have done or messed up on, please feel free to change anything and let me know!

---
+++ NovaTale/menustart1.gd
@@ -0,0 +1,18 @@
+extends Control
+
+func _ready():
+	$Panel/VBoxContainer/Start.grab_focus()
+
+
+func _on_exit_pressed():
+	get_tree().quit()
+
+
+func _on_continue_pressed():
+	pass #contiunue on a later date
+
+
+
+
+func _on_start_pressed():
+	get_tree().change_scene_to_file("res://room001.tscn")
---
+++ NovaTale/test_change_scene2.gd
@@ -0,0 +1,8 @@
+extends Area2D
+
+@onready var Player = "res://player.tscn"
+@onready var animation = $CharacterBody2D/CollisionShape2D/FriskDownWalk/AnimationPlayer
+
+func _on_body_entered(Player):
+	
+	get_tree().change_scene_to_file("res://Room001_1.tscn")
