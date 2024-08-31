233abc68c30a774894850e0500c247c8fadb455a
Noah <cramernoah8@gmail.com>
2024-08-30 22:58:29 -0400

    v.0.0.2
    An undertale fan project called NovaTale made by NovaBrooks

---
+++ NovaTale/scripts/test_change_scene2.gd
@@ -0,0 +1,7 @@
+extends Area2D
+
+@onready var Player = "res://player.tscn"
+@onready var animation = $CharacterBody2D/CollisionShape2D/FriskDownWalk/AnimationPlayer
+
+func _on_body_entered(_player):
+	get_tree().change_scene_to_file("res://scenes/Room001_1.tscn")
