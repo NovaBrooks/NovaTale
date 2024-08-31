251720e079417db47891c72a2fb5a3cf82a4b1dd
Noah <cramernoah8@gmail.com>
2024-08-31 16:40:21 -0400

    Add files via upload

--- NovaTale/scripts/CharacterBody2D.gd
+++ NovaTale/scripts/CharacterBody2D.gd
@@ -2,11 +2,17 @@ extends CharacterBody2D
 
 @export var speed = 100
 @onready var animation = $CollisionShape2D/FriskDownWalk/AnimationPlayer
-@onready var textbox_test1 = $"CollisionShape2D/FriskDownWalk/Camera2D/text box test"
+@onready var menu1test1 = $CollisionShape2D/FriskDownWalk/Camera2D/Control
 
+func _ready():
+	pass
 func handleInput():
 	var movedirection = Input.get_vector("ui_left","ui_right","ui_up","ui_down")
 	velocity = movedirection * speed
+
+func Close_game_with_esc():
+	if Input.is_action_just_pressed("ui_cancel"):
+		get_tree().quit()
 
 func updateAnimation():
 	if velocity.length() == 0:
@@ -18,6 +24,14 @@ func updateAnimation():
 		elif velocity.x > 0: direction = "walk_right"
 		elif velocity.y < 0: direction = "walk_up"	
 		animation.play(direction)
+
+func show_menu_c():
+	if menu1test1.visible == false:
+		if Input.is_action_just_pressed("c_for_cancel"):
+			menu1test1.set_visible(true)
+	elif menu1test1.visible == true:
+		if Input.is_action_just_pressed("c_for_cancel"):
+			menu1test1.set_visible(false)
 
 #func show_textbox1():
 #	if textbox_test1.visible == false:
@@ -31,4 +45,5 @@ func _physics_process(_delta):
 	handleInput()
 	move_and_slide()
 	updateAnimation()
-	#show_textbox1()
+	show_menu_c()
+	Close_game_with_esc()
